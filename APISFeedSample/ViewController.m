8ec579965af8ba18e7213017e29e457924f93cdb
Yoji Ueda <bluezbz@gmail.com>
2014-11-05 10:48:40 +0900

    Initial Commit

---
+++ APISFeedSample/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  APISFeedSample
+//
+//  Created by 植田 洋次 on 2014/11/05.
+//  Copyright (c) 2014年 Yoji Ueda. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
